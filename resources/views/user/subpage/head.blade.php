dea0f521eaa518116e387245e1c746b6052fa394
hoanghongthuy <hoanghongthuy17@gmail.com>
2020-10-19 00:06:55 +0700

    update

--- resources/views/user/subpage/head.blade.php
+++ resources/views/user/subpage/head.blade.php
@@ -1,13 +1,15 @@
-<head>
-    <meta charset="UTF-8">
-    <title>The Coffee House</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#03a6f3">
-    <link rel="stylesheet" href="{{url('Coffee')}}/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{url('Coffee')}}/croppie.css" />
-    <link rel="stylesheet" type="text/css" href="{{url('Coffee')}}/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="{{url('Coffee')}}/css/cake.css">
-    <link rel="stylesheet" href="{{url('Coffee')}}/css/styles.css">
-</head>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>The Coffee House</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#03a6f3">
+        <link rel="stylesheet" href="{{url('Coffee')}}/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{url('Coffee')}}/croppie.css" />
+        <link rel="stylesheet" type="text/css" href="{{url('Coffee')}}/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="{{url('Coffee')}}/css/cake.css">
+        <link rel="stylesheet" href="{{url('Coffee')}}/css/styles.css">
+    </head>
